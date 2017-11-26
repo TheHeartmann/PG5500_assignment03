967e7a9b4408b971a4f3e4f24abc69888a121877
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-11-26 20:36:50 +0100

    Prettify the index calculation in HeadlineScreen

--- lib/Display/src/HeadlineScreen.h
+++ lib/Display/src/HeadlineScreen.h
@@ -44,7 +44,7 @@ class HeadlineScreen : public NewsScreen
         _tft->setTextColor(_textColor);
         _tft->setTextSize(2);
         writeHeader();
-        _headlineRect.update(_headlines[_nextIndex]);
+        next();
     }
 
     void next()
@@ -58,13 +58,9 @@ class HeadlineScreen : public NewsScreen
   protected:
     void incrementIndex()
     {
-        ++_nextIndex;
-        if (_nextIndex >= _headlines.size())
-        {
-            _nextIndex = 0;
-        }
+        _nextIndex = (_nextIndex + 1) % _headlines.size();
     }
-    int _nextIndex = 0;
+    int _nextIndex = -1;
     bool _hasData = false;
     std::vector<Headline> _headlines{DEFAULT_HEADLINE};
     HeadlineRect _headlineRect = HeadlineRect(*_tft, _contentPosition, _contentDimensions, _headlines[_nextIndex]);
